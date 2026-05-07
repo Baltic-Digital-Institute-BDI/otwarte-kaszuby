87ce9567f7589c13289b831ffe2d6b34f2220681
Adam-it-baltic <it@baltic-digital.org>
2026-05-07 20:10:18 +0200

    Add basePath and assetPrefix to next.config.mjsfeat: add basePath/assetPrefix for Multi Zones (bdi.technology/otwartekaszuby)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/otwartekaszuby',
+  assetPrefix: '/otwartekaszuby',
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
